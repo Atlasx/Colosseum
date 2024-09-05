9d3de425e180752bda27e2338c923a1f1f6e91af
Atlasx <cjdouglas96@gmail.com>
2024-09-04 18:06:51 -1000

    Add GLAD for OpenGL functions

--- source/Main/main.cpp
+++ source/Main/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
 #include "engine.h"
+
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
 
 int main() {
 	std::cout << "Hello Main!" << std::endl;
 
+	// GLFW Setup
 	if (!glfwInit()) {
 		std::cerr << "Failed to initialize GLFW" << std::endl;
 		return -1;
@@ -15,27 +21,58 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Use the core profile
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Colosseum", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Colosseum", nullptr, nullptr);
 	if (window == nullptr) {
 		std::cerr << "Failed to create a GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-
 	glfwMakeContextCurrent(window);
+
+	// GLAD Setup
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
 	glfwSwapInterval(1);
 	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 
+	// ImGui Setup
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
+
+	// Engine Setup
 	CEngine my_engine;
 	my_engine.HelloWorld();
 
 	while (!glfwWindowShouldClose(window)) {
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
 		glfwPollEvents();
+
+		ImGui::ShowDemoWindow();
+		ImGui::Render();
 
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
 		glfwSwapBuffers(window);
 	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
