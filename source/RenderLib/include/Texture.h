e4f505401a6d39ba78e9f178fbad7793d2e408ec
Atlasx <cjdouglas96@gmail.com>
2024-09-09 19:10:43 -1000

    Half hearted work on textures

---
+++ source/RenderLib/include/Texture.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "glad/glad.h"
+#include "stb_image.h"
+#include <filesystem>
+
+class Texture
+{
+public:
+	Texture(std::filesystem::path);
+
+	void load();
+
+
+	void useTexture(GLuint slot);
+
+private:
+	GLuint textureID;
+	GLuint width, height;
+};
