adda5507162f03324bbf346dc68e1980fb952a21
Atlasx <cjdouglas96@gmail.com>
2024-12-16 20:00:58 -1000

    Begin EventSystem work

--- source/Engine/include/Systems/EventSystem.h
+++ source/Engine/include/Systems/EventSystem.h
@@ -7,8 +7,24 @@
 
 namespace CE
 {
+	/*
+	* Event System Notes
+	* 
+	* Core Features:
+	*  - Declare new events (dynamic or static?) (variable expected params?)
+	*  - Register listener for global events
+	*  - Register listener for targeted events (subset of global with ptr param?)
+	*  - Trigger events (multithreaded triggers?)
+	*  - Process all triggered events (stores event data until firing callback?)
+	*  - Remove event listeners (handle for tracking? find by object and type?)
+	*  - Pass values or arbitrary struct data through events (maximums?)
+	*  - Prerequisite tags checked on listener or before fire?
+	*/
 	class EventSystem : public EngineSystem
 	{
+	public:
+
+
 		// More friendship to allow Engine to access protected functions on derived class pointers
 		friend class Engine;
 		
