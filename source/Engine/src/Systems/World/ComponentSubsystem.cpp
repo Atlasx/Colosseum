f438cf430dfd44c9fe8cb1c9017940e6abaa7cc8
Atlasx <cjdouglas96@gmail.com>
2025-02-05 19:39:53 -1000

    Change formatting of Component Debug window

--- source/Engine/src/Systems/World/ComponentSubsystem.cpp
+++ source/Engine/src/Systems/World/ComponentSubsystem.cpp
@@ -7,6 +7,20 @@ namespace CE
 	void ComponentSubsystem::DrawComponentPools()
 	{
 		// Already within an imgui window context 
+		for (std::size_t i = 0; i < m_componentIDs.size(); i++)
+		{
+			ImGui::PushID(m_componentIDs[i].hash_code());
+			ImGui::Text(m_componentNames[i].c_str());
+
+			PoolInfo pi = m_componentStorage[m_componentIDs[i]]->GetPoolInfo();
+			const float percentFull = pi.maxCount > 0 ? (float)pi.currentCount / (float)pi.maxCount : 0.f;
+
+			char barBuffer[16];
+			sprintf(barBuffer, "%zu/%zu", pi.currentCount, pi.maxCount);
+			ImGui::ProgressBar(percentFull, ImVec2(0,0), barBuffer);
+			ImGui::PopID();
+		}
+
 		if (ImGui::CollapsingHeader("Registered Components"))
 		{
 			for (std::size_t i = 0; i < m_componentIDs.size(); i++)
@@ -15,23 +29,5 @@ namespace CE
 				ImGui::Text("%d: %s", m_componentIDs[i], m_componentNames[i].c_str());
 			}
 		}
-
-		if (ImGui::CollapsingHeader("Component Counts"))
-		{
-			for (std::size_t i = 0; i < m_componentIDs.size(); i++)
-			{
-				ImGui::Text(m_componentNames[i].c_str());
-
-				PoolInfo pi = m_componentStorage[m_componentIDs[i]]->GetPoolInfo();
-				const float percentFull = pi.maxCount > 0 ? (float)pi.currentCount / (float)pi.maxCount : 0.f;
-
-				char barBuffer[16];
-				sprintf(barBuffer, "%zu/%zu", pi.currentCount, pi.maxCount);
-				ImGui::ProgressBar(percentFull, ImVec2(0,0), barBuffer);
-			}
-		}
-
-
-
 	}
 }
